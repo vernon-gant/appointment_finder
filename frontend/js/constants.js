c7aee23a1ae152fc00112678029f1240c8fe8e74
Aleksandr Zakharov <aleks.zakharov@protonmail.com>
2024-01-10 19:57:02 +0100

    Changed IP

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -5,4 +5,4 @@ export const form = document.getElementById('appointment-form');
 
 export const toggleFormButton=document.getElementById('toggleForm');
 
-export const apiUrl = 'http://54.85.178.50:8080';
+export const apiUrl = 'http://44.212.69.161:8080';
